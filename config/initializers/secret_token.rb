ae241ac393e71f3b76e6961951ea04b64513dbdc
Mekbeb Worku <mekbeb@gmail.com>
2012-10-19 20:09:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dankira2::Application.config.secret_token = 'b75fa52c604d0f7b959701559a0be70c7530f42e95ea2aa6cd6b2d0d3370948d6a67bb2eacfc821d4c083891d9c1e5dbe1ce9ad3b38d2d045ee9e6146cfefbf6'
