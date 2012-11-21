3e99248e16fa56d8f43942eef0d13e778c9b086e
Mekbeb Worku <mekbeb@gmail.com>
2012-11-20 21:33:55 -0500

    done with creating three pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home"
   end
 
   def create
+  	@title = "Create"
   end
 
   def register
+  	@title = "Register"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe PagesController do
+render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do
+			get 'home'
+			response.should have_selector("title",
+			:content => "Gullit | Home")
+		end
+
   end
 
   describe "GET 'create'" do
@@ -14,6 +21,11 @@ describe PagesController do
       get 'create'
       response.should be_success
     end
+    it "should have the right title" do
+			get 'create'
+			response.should have_selector("title",
+			:content => "Gullit | Create")
+		end
   end
 
   describe "GET 'register'" do
@@ -21,6 +33,11 @@ describe PagesController do
       get 'register'
       response.should be_success
     end
+    it "should have the right title" do
+			get 'register'
+			response.should have_selector("title",
+			:content => "Gullit | Register")
+		end
   end
 
 end
